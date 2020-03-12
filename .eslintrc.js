b711c04b8e6ae6bc50fb98ec0032e58a9f583f4c
Nikhil Anand <mail@nikhil.io>
2020-03-12 09:15:20 -0500

    Linting, remove unnecessary deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,5 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-  },
+  extends: ['node', 'airbnb-base'],
 };
--- serve.js
+++ serve.js
@@ -1,15 +1,15 @@
-/* eslint-disable no-console */
+/* eslint-disable no-console, no-return-assign, no-unused-vars */
 
-const chalk = require("chalk");
-const cors = require("cors");
-const express = require("express");
-const morgan = require("morgan");
+const chalk = require('chalk');
+const cors = require('cors');
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.port || 8000;
 const MIN_DELAY_IN_MS = 10;
 const MAX_DELAY_IN_MS = 3000;
-const SUCCESS_THRESHOLD = 0.75; // I.e. "Succeed ~75% of the time"
+const SUCCESS_THRESHOLD = 0.75; // I.e. 'Succeed ~75% of the time'
 
 /**
  *
@@ -17,10 +17,10 @@ const SUCCESS_THRESHOLD = 0.75; // I.e. "Succeed ~75% of the time"
  *
  */
 
-const data = require("./data.json");
+const data = require('./data.json');
 
 const listOfFields = {
-  fields: Object.keys(data).map(id => ({
+  fields: Object.keys(data).map((id) => ({
     id,
     name: data[id].name,
     type: data[id].type,
@@ -37,7 +37,7 @@ const responseDelay = () => {
   let delay = 0;
 
   return {
-    set: _delay => (delay = _delay),
+    set: (_delay) => (delay = _delay),
     get: () => delay,
   };
 };
@@ -52,24 +52,22 @@ const delay = responseDelay();
 
 const iShouldRandomlyActUp = () => Math.random() >= SUCCESS_THRESHOLD;
 
-const randomDelayInMilliSeconds = () =>
-  Math.floor(
-    Math.random() * (MAX_DELAY_IN_MS - MIN_DELAY_IN_MS) + MIN_DELAY_IN_MS,
-  );
+const randomDelayInMilliSeconds = () => Math.floor(
+  Math.random() * (MAX_DELAY_IN_MS - MIN_DELAY_IN_MS) + MIN_DELAY_IN_MS,
+);
 
 const queryParamMiddleware = (req, res, next) => {
-  delay.set("fast" in req.query ? 0 : randomDelayInMilliSeconds());
+  delay.set('fast' in req.query ? 0 : randomDelayInMilliSeconds());
 
-  if ("succeed" in req.query) {
+  if ('succeed' in req.query) {
     return setTimeout(() => next(), delay.get());
   }
 
-  if (iShouldRandomlyActUp() || "fail" in req.query) {
+  if (iShouldRandomlyActUp() || 'fail' in req.query) {
     return setTimeout(
-      () =>
-        res.status(500).send({
-          message: "I failed on a whim. I am capricious. That's how I roll.",
-        }),
+      () => res.status(500).send({
+        message: 'I failed on a whim. I am capricious. That\'s how I roll.',
+      }),
       delay.get(),
     );
   }
@@ -89,7 +87,7 @@ app.use(
   morgan(
     (tokens, req, res) => {
       const status = res.statusCode;
-      const responseTime = tokens["response-time"](req, res);
+      const responseTime = tokens['response-time'](req, res);
 
       /**
        * In morgan, the response time is `undefined` if the
@@ -98,36 +96,35 @@ app.use(
        */
       const responseChunk = responseTime
         ? `${responseTime}ms`
-        : "(interrupted)";
+        : '(interrupted)';
 
       return [
-        chalk.yellow(`[${tokens.date(req, res, "iso")}]`),
+        chalk.yellow(`[${tokens.date(req, res, 'iso')}]`),
         chalk.blue(`${tokens.method(req, res)}`),
         tokens.url(req, res),
         status === 200 || status === 304
           ? chalk.green(status)
           : chalk.red(status),
         chalk.gray(`${responseChunk}`),
-        delay.get() > 0 ? chalk.gray("(delayed)") : "",
-      ].join(" ");
+        delay.get() > 0 ? chalk.gray('(delayed)') : '',
+      ].join(' ');
     },
     {
-      skip: (req, res) => req.url === "/favicon.ico",
+      skip: (req, res) => req.url === '/favicon.ico',
     },
   ),
 );
 
+
+app.get('/', (_, res) => res.send({
+  message: 'Why hello! Have you read the README?',
+}));
+
 app.use(queryParamMiddleware);
 
-app.get("/", (_, res) =>
-  res.send({
-    message: "Why hello! Have you read the README?",
-  }),
-);
+app.get('/fields', (req, res) => res.send(listOfFields));
 
-app.get("/fields", (req, res) => res.send(listOfFields));
-
-app.get("/fields/:id", (req, res) => {
+app.get('/fields/:id', (req, res) => {
   const { id } = req.params;
 
   if (listOfFieldIds.indexOf(id) === -1) {
@@ -139,12 +136,10 @@ app.get("/fields/:id", (req, res) => {
   return res.send(data[id]);
 });
 
-app.use((_, res) =>
-  res.status(404).send({
-    message: "Could not find that resource.",
-  }),
-);
+app.use((_, res) => res.status(404).send({
+  message: 'Could not find that resource.',
+}));
 
-app.listen(port, () =>
-  console.log(`🚀 The Fake Fields API is listening on port ${port}`),
-);
+app.listen(port, () => console.log(
+  `🚀 The Fake Fields API is listening on port ${port}`,
+));
